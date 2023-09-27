0cfb33b138526f362995d9a89a02a56c0ac56a40
Dmtr Smirnov <d.smirnov.vo@mail.ru>
2023-09-27 19:39:54 +0300

    закончил оформление footer

--- tW.js
+++ tW.js
@@ -1,22 +1,24 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
-// Вызов и закрытие модального окна "Связаться"
-const modalBtn = document.querySelector('.contact'),
-modalWindow = document.querySelector('.modal');
+    // Вызов и закрытие модального окна "Связаться"
+    const modalBtn = document.querySelectorAll('.contact'),
+    modalWindow = document.querySelector('.modal');
 
-modalBtn.addEventListener('click', ()=>{
-    modalWindow.classList.add('modal_show');
-});    
+    modalBtn.forEach(item => {
+        item.addEventListener('click', ()=>{
+            modalWindow.classList.add('modal_show');
+        }); 
+    });   
 
-modalWindow.addEventListener('click', (e)=>{
-if(e.target === modalWindow || e.target.getAttribute('data-close') == ""){
+    modalWindow.addEventListener('click', (e)=>{
+    if(e.target === modalWindow || e.target.getAttribute('data-close') == ""){
+        modalWindow.classList.remove('modal_show');
+    }
+    });
+    document.addEventListener('keydown', (e)=>{
+    if (e.code === 'Escape'){
     modalWindow.classList.remove('modal_show');
-}
-});
-document.addEventListener('keydown', (e)=>{
-if (e.code === 'Escape'){
-   modalWindow.classList.remove('modal_show');
-}
-});
+    }
+    });
 
 });
